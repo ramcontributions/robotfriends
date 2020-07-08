6eddbfa665e133b4cd9fbe0c29b9f119b0bf0592
Ram <ram.2005.p@gmail.com>
2020-07-08 20:43:43 +0530

    with redux thunk

--- src/reducers.js
+++ src/reducers.js
@@ -1,10 +1,15 @@
-import { CHNAGE_SEARCH_FIELD } from "./constants";
+import { 
+    CHNAGE_SEARCH_FIELD,
+    REQUEST_ROBOTS_PENDING,
+    REQUEST_ROBOTS_SUCCESS,
+    REQUEST_ROBOTS_FAILED,
+} from "./constants";
 
-const initialState = {
+const initialStateSearch = {
     searchField : ""
 }
 
-export const searchRobots = (state=initialState, action={}) => {
+export const searchRobots = (state = initialStateSearch, action={}) => {
     switch (action.type){
         case CHNAGE_SEARCH_FIELD : 
             return Object.assign({}, state, {searchField : action.payload})
@@ -12,4 +17,23 @@ export const searchRobots = (state=initialState, action={}) => {
             return state
     }
 
+}
+
+const initialStateRobots = {
+    isPending :  false,
+    robots : [],
+    error : ''
+}
+
+export const requestRobots = (state = initialStateRobots, action = {}) => {
+    switch(action.type){
+        case REQUEST_ROBOTS_PENDING :
+            return Object.assign({}, state, {isPending : true})
+        case REQUEST_ROBOTS_SUCCESS :
+            return Object.assign({}, state, {isPending : false, robots : action.payload})
+        case REQUEST_ROBOTS_FAILED :
+            return Object.assign({}, state, {isPending : false, error : action.payload})
+        default :
+            return state
+    }
 }
